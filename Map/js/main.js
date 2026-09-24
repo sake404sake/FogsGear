0c5e770f03be737168d5c5abfcd9b5be17e314d6
sake404sake <sake404sake@gmail.com>
2026-09-24 21:12:54 +0900

    Add files via upload

--- Map/js/main.js
+++ Map/js/main.js
@@ -1024,4 +1024,10 @@ if (minZoomSelect) {
     });
 }
 
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('./sw.js', { scope: './' }).catch(() => {});
+    });
+}
+
 init();
